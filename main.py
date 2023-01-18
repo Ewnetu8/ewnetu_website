4c077f22e321ab10d53624a00f389ff440cb84f8
Ewnetu Abebe <58074054+Ewnetu8@users.noreply.github.com>
2023-01-18 08:52:39 +0000

    flask app

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def Heloo_world():
+  return "Hello Ewnetu"
+
+if __name__ == "__main__":
+  #print('I am inside the if now')
+  app.run(host = '0.0.0.0', debug = True)
